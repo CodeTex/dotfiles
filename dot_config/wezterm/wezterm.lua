537f896971ed45965d9fd1973a37d73bf69b9e91
Julian Dittrich <julian.dittrich@siemens.com>
2026-03-21 11:37:29 +0100

    feat: update dotfiles

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -217,18 +217,18 @@ config.harfbuzz_features = {
 }
 
 -- Keybindings
-config.leader = { key = "f", mods = "ALT", timeout_milliseconds = 2500 }
+config.leader = { key = "f", mods = "ALT", timeout_milliseconds = 2000 }
 config.keys = {
 	{ mods = "LEADER", key = "c", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-	{ mods = "LEADER", key = "w", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+	{ mods = "LEADER", key = "x", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
 	{ mods = "LEADER", key = "b", action = wezterm.action.ActivateTabRelative(-1) },
 	{ mods = "LEADER", key = "n", action = wezterm.action.ActivateTabRelative(1) },
-	{ mods = "LEADER", key = ",", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ mods = "LEADER", key = ".", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ mods = "LEADER", key = "h", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ mods = "LEADER", key = "j", action = wezterm.action.ActivatePaneDirection("Down") },
-	{ mods = "LEADER", key = "k", action = wezterm.action.ActivatePaneDirection("Up") },
-	{ mods = "LEADER", key = "l", action = wezterm.action.ActivatePaneDirection("Right") },
+	{ mods = "LEADER", key = "i", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "e", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ mods = "LEADER", key = "j", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ mods = "LEADER", key = "k", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ mods = "LEADER", key = "l", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ mods = "LEADER", key = ";", action = wezterm.action.ActivatePaneDirection("Right") },
 	{ mods = "LEADER", key = "LeftArrow", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
 	{ mods = "LEADER", key = "DownArrow", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
 	{ mods = "LEADER", key = "UpArrow", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
