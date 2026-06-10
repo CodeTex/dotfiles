d1d8099e9ee3bf3363fc556795b45cd77b56a1dc
Julian Dittrich <julian.dittrich@siemens.com>
2026-06-10 19:17:46 +0200

    feat: update dotfiles

--- dot_config/powershell/prompt.ps1
+++ dot_config/powershell/prompt.ps1
@@ -5,11 +5,13 @@
 $script:ConfigRoot = Split-Path -Parent $PSCommandPath
 . "$ConfigRoot\functions\prompt-helpers.ps1"
 
-# Invoke-CachedShellTool `
-#     -ToolName "mise" `
-#     -Arguments @("activate", "pwsh") `
-#     -CachePath "$HOME\.cache\powershell\mise-activate-pwsh.ps1" `
-#     -InstallHint "Install with: scoop install mise"
+Invoke-CachedShellTool `
+    -ToolName "mise" `
+    -Arguments @("activate", "pwsh") `
+    -CachePath "$HOME\.cache\powershell\mise-activate-pwsh.ps1" `
+    -InstallHint "Install with: scoop install mise"
+
+# (&mise activate pwsh) | Out-String | Invoke-Expression
 
 Invoke-CachedShellTool `
     -ToolName "zoxide" `
@@ -17,10 +19,12 @@ Invoke-CachedShellTool `
     -CachePath "$HOME\.cache\powershell\zoxide-init-powershell.ps1" `
     -InstallHint "Install with: mise use -g zoxide"
 
+# Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
 Invoke-CachedShellTool `
     -ToolName "starship" `
     -Arguments @("init", "powershell", "--print-full-init") `
     -CachePath "$HOME\.cache\powershell\starship-init-powershell.ps1" `
     -InstallHint "Install with: mise use -g starship"
 
-    
+# Invoke-Expression (&starship init powershell)
